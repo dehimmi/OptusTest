4ec54d2a74de728d1e92b2f7dc8946a87e5c4404
Deepak Ahuja <deepakahuja79@gmail.com>
2020-09-05 21:40:24 +1000

    Optus Availability POC

---
+++ OptusTest.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+def test_pass():
+    assert 1+1 == 2
+
+
+#client.write_points(result, database='pythondb', time_precision='ms')
+
+# python -m pytest test_example.py --pytest-influxdb --influxdb_host=localhost --influxdb_name=pythondb
+
+# python -m pytest test_example.py --pytest-influxdb --influxdb_host=52.63.126.230 --influxdb_name=pythondb
